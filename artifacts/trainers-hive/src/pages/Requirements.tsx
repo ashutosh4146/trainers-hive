6d6518945655e215a66b0738e958a97d982f683b
ashutosh4146 <47911570+ashutosh4146@users.noreply.github.com>
2026-06-09 12:40:26 +0530

    Remove unused requirements import

--- artifacts/trainers-hive/src/pages/Requirements.tsx
+++ artifacts/trainers-hive/src/pages/Requirements.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from "react";
+import React, { useState, useRef } from "react";
 import { Link, useLocation } from "wouter";
 import {
   useListRequirements,
